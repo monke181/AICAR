f3210000ad21512319f5106498f812b43eccdc97
iRedona516 <iredona@uci.edu>
2024-02-01 16:26:06 -0800

    Test Commit

--- Python AI/AICARexample.py
+++ Python AI/AICARexample.py
@@ -11,6 +11,7 @@ SCREEN_HEIGHT = 1016
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 TRACK = pygame.image.load(os.path.join("track.png"))
 
+
 class Car(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
